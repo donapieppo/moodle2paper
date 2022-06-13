b0baf2788a2afc41f1a9212d910309c4eedfdcd2
Pietro <donapieppo@yahoo.it>
2022-06-13 16:44:06 +0200

    total

--- parsa.py
+++ parsa.py
@@ -4,19 +4,17 @@ import glob
 from lib.exercise import Exercise, Answer, Tipology
 
 tipologies = []
+total = ""
 
-for moodle_file in glob.glob('../stefano/db/*.xml'):
+for moodle_file in glob.glob('test/*.xml'):
     tipology = Tipology(moodle_file)
     for e in tipology.choose_random(3):
-        print(e)
+        total = total + str(e)
 
-
-exit(0)
-with open('res.tex', 'w') as result:
+with open('result/res.tex', 'w') as result:
     with open('tex/header.tex', 'r') as f:
         result.write(f.read())
-    for n, e in enumerate(exercises):
-        result.write(str(e))
+    result.write(total)
     with open('tex/footer.tex', 'r') as f:
         result.write(f.read())
 
